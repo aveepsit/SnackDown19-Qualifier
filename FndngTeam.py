56a09f4ea6ea1572a51656f1ad2a2810a09cbb27
Aveepsit <aveepsitchowdhury@gmail.com>
2018-10-15 19:19:44 +0530

    add solution to problem 'Finding Teammate'

--- FndngTeam.py
+++ FndngTeam.py
@@ -1,10 +1,3 @@
-ncrLib = {}
-
-def nc2(n):
-    c = (n*(n-1))//2
-    ncrLib[n] = c
-    return c
-
 for testcase in range(int(input())):
 
     n = int(input())
@@ -18,22 +11,21 @@ for testcase in range(int(input())):
             dict[no] = dict[no] + 1
         except:
             dict[no] = 1
-
     dict = list(dict.items())
     dict.sort(key=lambda x: x[0], reverse=True)
     dict = [x[1] for x in dict]
 
     for ind in range(len(dict)):
-        while dict[ind]>0:
-            if dict[ind]>1:
-                try:
-                    comb = (comb * ncrLib[dict[ind]])%m
-                except:
-                    comb = (comb * nc2(dict[ind]))%m
-                dict[ind] -= 2
-            else:
-                comb = (comb * dict[ind+1])%m
-                dict[ind+1] -= 1
-                dict[ind] = 0
+        if dict[ind]==0:
+            continue
+        if (dict[ind]%2==0):
+            for j in range(dict[ind]-1,2,-2):
+                comb = (comb*j) % m
+        else:
+            for j in range(dict[ind],2,-2):
+                comb = (comb*j) % m
+
+            comb = (comb*dict[ind+1]) % m
+            dict[ind+1] -= 1
 
     print(comb)
